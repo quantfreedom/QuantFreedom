caf17add54684c7febe60897c1f6685a60e56e32
QuantFreedom1022 <quantfreedom1022@gmail.com>
2023-04-06 12:34:34 -0700

    made all evaluators graphable and multicoin

--- quantfreedom/evaluators/evaluators.py
+++ quantfreedom/evaluators/evaluators.py
@@ -1,43 +1,262 @@
 import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
-from quantfreedom._typing import pdFrame, Union, Array1d, Optional, pdSeries
+from itertools import product
+from plotly.subplots import make_subplots
+from quantfreedom._typing import pdFrame, Union, Array1d, Optional
 
 
 def combine_evals(
-    bigger_df: pdFrame,
-    smaller_df: pdFrame,
-):
-    bigger_df_values = bigger_df.values
-    smaller_df_values = smaller_df.values
+    first_eval_data: pdFrame,
+    second_eval_data: pdFrame,
+    plot_results: bool = False,
+    first_eval_data_needs_prices: bool = False,
+    second_eval_data_needs_prices: bool = False,
+    prices: pdFrame = None,
+    first_ind_data: pdFrame = None,
+    second_ind_data: pdFrame = None,
+) -> pdFrame:
+
+    if plot_results and (first_ind_data is None or second_ind_data is None):
+        raise ValueError(
+            "Make sure you are sending first and second indicator data if you want to plot")
+    elif plot_results:
+        if first_eval_data_needs_prices and prices is None:
+            raise ValueError("You need to provide prices to plot the candles")
+        elif not first_eval_data_needs_prices and prices is not None:
+            raise ValueError(
+                "Make sure you set first_eval_data_needs_prices to true if you want to send price data")
+    elif not plot_results and (
+        first_eval_data_needs_prices or
+        prices is not None or
+        first_ind_data is not None or
+        second_ind_data is not None
+    ):
+        raise ValueError(
+            "Make sure you set plot_results to true if you want to send any plotting data.")
+
+    if len(first_eval_data.columns.levels) < len(second_eval_data.columns.levels):
+        temp = len(first_eval_data.columns.levels)
+    else:
+        temp = len(second_eval_data.columns.levels)
+
+    list_for_product = []
+    pd_col_names = []
+    for x in range(temp):
+        if list(first_eval_data.columns.levels[x]) == list(second_eval_data.columns.levels[x]):
+            list_for_product.append(list(first_eval_data.columns.levels[x]))
+            pd_col_names.append(first_eval_data.columns.names[x])
+    levels = list(product(*list_for_product))
+
+    pd_col_names = pd_col_names + list(first_eval_data.droplevel(pd_col_names, axis=1).columns.names) + \
+        list(second_eval_data.droplevel(pd_col_names, axis=1).columns.names)
+
     combine_array = np.empty(
-        (bigger_df.shape[0], bigger_df.shape[1] * smaller_df.shape[1]), dtype=np.bool_
+        (first_eval_data.shape[0], first_eval_data[levels[0]].shape[1] * second_eval_data[levels[0]].shape[1] * len(levels)), dtype=np.bool_
     )
-    counter = 0
+
+    try:
+        second_eval_data[levels[0]].columns[0][0]
+        temp_smaller_def_columns = list(second_eval_data[levels[0]].columns)
+    except:
+        temp_smaller_def_columns = []
+        for value in list(second_eval_data[levels[0]].columns):
+            temp_smaller_def_columns.append((value,))
+
+    try:
+        first_eval_data[levels[0]].columns[0][0]
+        temp_big_def_columns = list(first_eval_data[levels[0]].columns)
+    except:
+        temp_big_def_columns = []
+        for value in list(first_eval_data[levels[0]].columns):
+            temp_big_def_columns.append((value,))
+
+    comb_counter = 0
     pd_multind_tuples = ()
-    pd_col_names = list(bigger_df.columns.names) + \
-        list(smaller_df.columns.names)
+    for level in levels:
+        temp_big_df = first_eval_data[level]
+        temp_small_df = second_eval_data[level]
+        for big_count, big_values in enumerate(temp_big_df.values.T):
 
-    for big_col in range(bigger_df.shape[1]):
-        temp_bigger_df_array = bigger_df_values[:, big_col]
-        for small_col in range(smaller_df.shape[1]):
-            temp_smaller_df_array = smaller_df_values[:, small_col]
+            for small_count, small_values in enumerate(temp_small_df.values.T):
 
-            combine_array[:, counter] = np.logical_and(
-                temp_bigger_df_array == True, temp_smaller_df_array == True
+                combine_array[:, comb_counter] = np.logical_and(
+                    big_values == True, small_values == True
+                )
+
+                pd_multind_tuples = pd_multind_tuples + (
+                    level + temp_big_def_columns[big_count] +
+                    temp_smaller_def_columns[small_count],
+                )
+
+                comb_counter += 1
+
+    if plot_results:
+        plot_index = second_eval_data.index
+
+        temp_first_ind_data = first_ind_data.iloc[:, -1]
+        temp_second_ind_data = second_ind_data.iloc[:, -1]
+
+        temp_combine_array = combine_array[:, -1]
+
+        # candle data with subplot
+        if first_eval_data_needs_prices and not second_eval_data_needs_prices:
+            temp_prices = prices[list(prices.columns)[-1][0]]
+            fig = make_subplots(
+                rows=2,
+                cols=1,
+                shared_xaxes=True,
             )
-
-            pd_multind_tuples = pd_multind_tuples + (
-                bigger_df.columns[big_col] + smaller_df.columns[small_col],
+            fig.add_traces(
+                data=[
+                    go.Candlestick(
+                        x=plot_index,
+                        open=temp_prices.open,
+                        high=temp_prices.high,
+                        low=temp_prices.low,
+                        close=temp_prices.close,
+                        name="Candles",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#60BFE1'),
+                        name="First Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_first_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+                rows=1,
+                cols=1,
             )
+            fig.add_traces(
+                data=[
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=2, color='#60BFE1'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+                rows=2,
+                cols=1,
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=700, title="Last Column of the Results")
+            fig.show()
 
-            counter += 1
+        elif first_eval_data_needs_prices and second_eval_data_needs_prices:
+            temp_prices = prices[list(prices.columns)[-1][0]]
+            fig = go.Figure(
+                data=[
+                    go.Candlestick(
+                        x=plot_index,
+                        open=temp_prices.open,
+                        high=temp_prices.high,
+                        low=temp_prices.low,
+                        close=temp_prices.close,
+                        name="Candles",
+                    ),
+
+                    # First Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#60BFE1'),
+                        name="First Ind",
+                    ),
+
+                    # Second Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#84E5AC'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=500, title="Last Column of the Results")
+            fig.show()
+
+        elif not first_eval_data_needs_prices and not second_eval_data_needs_prices:
+            fig = go.Figure(
+                data=[
+                    # First Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=2, color='green'),
+                        name="First Ind",
+                    ),
+
+                    # Second Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='red'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=500, title="Last Column of the Results")
+            fig.show()
 
     return pd.DataFrame(
         combine_array,
-        index=bigger_df.index,
+        index=second_eval_data.index,
         columns=pd.MultiIndex.from_tuples(
-            tuples=list(pd_multind_tuples), names=pd_col_names
+            tuples=list(pd_multind_tuples),
+            names=pd_col_names,
         ),
     )
 
@@ -50,27 +269,6 @@ def eval_is_below(
     cand_ohlc: Optional[str] = None,
     plot_results: bool = False,
 ) -> pdFrame:
-    """eval_is_below _summary_
-
-    _extended_summary_
-
-    Parameters
-    ----------
-    want_to_evaluate : pdFrame
-        I think of this like I want to evaluate if the thing i am sending like EMA is below the price or is below the indicator data which could be rsi. So it would be i want to evalute if the EMA is below the RSI.
-    user_args : Optional[Union[list[int, float], int, float, Array1d]], optional
-        _description_, by default None
-    indicator_data : Optional[pdFrame], optional
-        _description_, by default None
-    prices : Optional[pdFrame], optional
-        _description_, by default None
-    cand_ohlc : Optional[str], optional
-        _description_, by default None
-
-    Returns
-    -------
-    pdFrame
-    """
     if not isinstance(want_to_evaluate, pdFrame):
         raise ValueError("Data must be a dataframe with multindex")
 
@@ -204,6 +402,15 @@ def eval_is_below(
             fig.update_layout(height=500, title="Last Column of the Results")
             fig.show()
 
+        return pd.DataFrame(
+            eval_array,
+            index=want_to_evaluate.index,
+            columns=pd.MultiIndex.from_tuples(
+                tuples=list(pd_multind_tuples),
+                names=pd_col_names,
+            ),
+        ).swaplevel(1, -1, axis=1)
+
     elif isinstance(indicator_data, pdFrame):
         want_to_evaluate_name = want_to_evaluate.columns.names[-1].split("_")[
             1]
@@ -313,7 +520,6 @@ def eval_is_below(
         raise ValueError(
             "something is wrong with what you sent please make sure the type of variable you are sending matches with the type required"
         )
-
     return pd.DataFrame(
         eval_array,
         index=want_to_evaluate.index,
@@ -323,6 +529,7 @@ def eval_is_below(
         ),
     )
 
+
 def eval_is_above(
     want_to_evaluate: pdFrame,
     user_args: Optional[Union[list[int, float], int, float, Array1d]] = None,
@@ -331,27 +538,6 @@ def eval_is_above(
     cand_ohlc: Optional[str] = None,
     plot_results: bool = False,
 ) -> pdFrame:
-    """eval_is_above _summary_
-
-    _extended_summary_
-
-    Parameters
-    ----------
-    want_to_evaluate : pdFrame
-        I think of this like I want to evaluate if the thing i am sending like EMA is above the price or is above the indicator data which could be rsi. So it would be i want to evalute if the EMA is above the RSI.
-    user_args : Optional[Union[list[int, float], int, float, Array1d]], optional
-        _description_, by default None
-    indicator_data : Optional[pdFrame], optional
-        _description_, by default None
-    prices : Optional[pdFrame], optional
-        _description_, by default None
-    cand_ohlc : Optional[str], optional
-        _description_, by default None
-
-    Returns
-    -------
-    pdFrame
-    """
     if not isinstance(want_to_evaluate, pdFrame):
         raise ValueError("Data must be a dataframe with multindex")
 
@@ -484,6 +670,14 @@ def eval_is_above(
             fig.update_xaxes(rangeslider_visible=False)
             fig.update_layout(height=500, title="Last Column of the Results")
             fig.show()
+            return pd.DataFrame(
+                eval_array,
+                index=want_to_evaluate.index,
+                columns=pd.MultiIndex.from_tuples(
+                    tuples=list(pd_multind_tuples),
+                    names=pd_col_names,
+                ),
+            ).swaplevel(1, -1, axis=1)
 
     elif isinstance(indicator_data, pdFrame):
         want_to_evaluate_name = want_to_evaluate.columns.names[-1].split("_")[
@@ -594,7 +788,6 @@ def eval_is_above(
         raise ValueError(
             "something is wrong with what you sent please make sure the type of variable you are sending matches with the type required"
         )
-
     return pd.DataFrame(
         eval_array,
         index=want_to_evaluate.index,
--- quantfreedom/testing.py
+++ quantfreedom/testing.py
@@ -4,56 +4,79 @@ import numpy as np
 from itertools import product
 import plotly.graph_objects as go
 from quantfreedom._typing import pdFrame, Optional, Array1d, Union
+from plotly.subplots import make_subplots
 
 
 def testing_combine_evals(
-    first_data: pdFrame,
-    second_data: pdFrame,
+    first_eval_data: pdFrame,
+    second_eval_data: pdFrame,
     plot_results: bool = False,
-    first_data_needs_prices: bool = False,
-    prices: Optional[pdFrame] = None
-):
-    if len(first_data.columns.levels) < len(second_data.columns.levels):
-        temp = len(first_data.columns.levels)
+    first_eval_data_needs_prices: bool = False,
+    second_eval_data_needs_prices: bool = False,
+    prices: pdFrame = None,
+    first_ind_data: pdFrame = None,
+    second_ind_data: pdFrame = None,
+) -> pdFrame:
+
+    if plot_results and (first_ind_data is None or second_ind_data is None):
+        raise ValueError(
+            "Make sure you are sending first and second indicator data if you want to plot")
+    elif plot_results:
+        if first_eval_data_needs_prices and prices is None:
+            raise ValueError("You need to provide prices to plot the candles")
+        elif not first_eval_data_needs_prices and prices is not None:
+            raise ValueError(
+                "Make sure you set first_eval_data_needs_prices to true if you want to send price data")
+    elif not plot_results and (
+        first_eval_data_needs_prices or
+        prices is not None or
+        first_ind_data is not None or
+        second_ind_data is not None
+    ):
+        raise ValueError(
+            "Make sure you set plot_results to true if you want to send any plotting data.")
+
+    if len(first_eval_data.columns.levels) < len(second_eval_data.columns.levels):
+        temp = len(first_eval_data.columns.levels)
     else:
-        temp = len(second_data.columns.levels)
+        temp = len(second_eval_data.columns.levels)
 
     list_for_product = []
     pd_col_names = []
     for x in range(temp):
-        if list(first_data.columns.levels[x]) == list(second_data.columns.levels[x]):
-            list_for_product.append(list(first_data.columns.levels[x]))
-            pd_col_names.append(first_data.columns.names[x])
+        if list(first_eval_data.columns.levels[x]) == list(second_eval_data.columns.levels[x]):
+            list_for_product.append(list(first_eval_data.columns.levels[x]))
+            pd_col_names.append(first_eval_data.columns.names[x])
     levels = list(product(*list_for_product))
 
-    pd_col_names = pd_col_names + list(first_data.droplevel(pd_col_names, axis=1).columns.names) + \
-        list(second_data.droplevel(pd_col_names, axis=1).columns.names)
+    pd_col_names = pd_col_names + list(first_eval_data.droplevel(pd_col_names, axis=1).columns.names) + \
+        list(second_eval_data.droplevel(pd_col_names, axis=1).columns.names)
 
     combine_array = np.empty(
-        (first_data.shape[0], second_data[levels[0]].shape[1] * first_data[levels[0]].shape[1] * len(levels)), dtype=np.bool_
+        (first_eval_data.shape[0], first_eval_data[levels[0]].shape[1] * second_eval_data[levels[0]].shape[1] * len(levels)), dtype=np.bool_
     )
-    
-    try :
-        second_data[levels[0]].columns[0][0]
-        temp_smaller_def_columns = list(second_data[levels[0]].columns)
+
+    try:
+        second_eval_data[levels[0]].columns[0][0]
+        temp_smaller_def_columns = list(second_eval_data[levels[0]].columns)
     except:
         temp_smaller_def_columns = []
-        for value in list(second_data[levels[0]].columns):
+        for value in list(second_eval_data[levels[0]].columns):
             temp_smaller_def_columns.append((value,))
-    
-    try :
-        first_data[levels[0]].columns[0][0]
-        temp_big_def_columns = list(first_data[levels[0]].columns)
+
+    try:
+        first_eval_data[levels[0]].columns[0][0]
+        temp_big_def_columns = list(first_eval_data[levels[0]].columns)
     except:
         temp_big_def_columns = []
-        for value in list(first_data[levels[0]].columns):
+        for value in list(first_eval_data[levels[0]].columns):
             temp_big_def_columns.append((value,))
 
     comb_counter = 0
     pd_multind_tuples = ()
     for level in levels:
-        temp_big_df = first_data[level]
-        temp_small_df = second_data[level]
+        temp_big_df = first_eval_data[level]
+        temp_small_df = second_eval_data[level]
         for big_count, big_values in enumerate(temp_big_df.values.T):
 
             for small_count, small_values in enumerate(temp_small_df.values.T):
@@ -69,47 +92,167 @@ def testing_combine_evals(
 
                 comb_counter += 1
 
-    if plot_results and prices is not None:
-        temp_first_data_values = first_data.iloc[:-1]
-        plot_index = second_data.index
+    if plot_results:
+        plot_index = second_eval_data.index
+        
+        temp_first_ind_data = first_ind_data.iloc[:, -1]
+        temp_second_ind_data = second_ind_data.iloc[:, -1]
 
-        fig = go.Figure(
-            data=[
-                go.Candlestick(
-                    x=plot_index,
-                    open=prices.open,
-                    high=prices.high,
-                    low=prices.low,
-                    close=prices.close,
-                    name="Candles",
-                ),
-                go.Scatter(
-                    x=plot_index,
-                    y=temp_first_data_values,
-                    mode="lines",
-                    line=dict(width=4, color='lightblue'),
-                    name="Name",
-                ),
-                go.Scatter(
-                    x=plot_index,
-                    y=np.where(
-                        combine_array[:, -1],
-                        temp_first_data_values,
-                        np.nan,
+        temp_combine_array = combine_array[:, -1]
+
+        # candle data with subplot
+        if first_eval_data_needs_prices and not second_eval_data_needs_prices:
+            temp_prices = prices[list(prices.columns)[-1][0]]
+            fig = make_subplots(
+                rows=2,
+                cols=1,
+                shared_xaxes=True,
+            )
+            fig.add_traces(
+                data=[
+                    go.Candlestick(
+                        x=plot_index,
+                        open=temp_prices.open,
+                        high=temp_prices.high,
+                        low=temp_prices.low,
+                        close=temp_prices.close,
+                        name="Candles",
                     ),
-                    mode="markers",
-                    marker=dict(size=3, color='yellow'),
-                    name="Signals",
-                ),
-            ]
-        )
-        fig.update_xaxes(rangeslider_visible=False)
-        fig.update_layout(height=500, title="Last Column of the Results")
-        fig.show()
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#60BFE1'),
+                        name="First Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_first_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+                rows=1,
+                cols=1,
+            )
+            fig.add_traces(
+                data=[
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=2, color='#60BFE1'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+                rows=2,
+                cols=1,
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=700, title="Last Column of the Results")
+            fig.show()
+        elif first_eval_data_needs_prices and second_eval_data_needs_prices:
+            temp_prices = prices[list(prices.columns)[-1][0]]
+            fig = go.Figure(
+                data=[
+                    go.Candlestick(
+                        x=plot_index,
+                        open=temp_prices.open,
+                        high=temp_prices.high,
+                        low=temp_prices.low,
+                        close=temp_prices.close,
+                        name="Candles",
+                    ),
 
+                    # First Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#60BFE1'),
+                        name="First Ind",
+                    ),
+
+                    # Second Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='#84E5AC'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=500, title="Last Column of the Results")
+            fig.show()
+        elif not first_eval_data_needs_prices and not second_eval_data_needs_prices:
+            fig = go.Figure(
+                data=[
+                    # First Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_first_ind_data,
+                        mode="lines",
+                        line=dict(width=2, color='green'),
+                        name="First Ind",
+                    ),
+
+                    # Second Plot
+                    go.Scatter(
+                        x=plot_index,
+                        y=temp_second_ind_data,
+                        mode="lines",
+                        line=dict(width=3, color='red'),
+                        name="Second Ind",
+                    ),
+                    go.Scatter(
+                        x=plot_index,
+                        y=np.where(
+                            temp_combine_array,
+                            temp_second_ind_data,
+                            np.nan,
+                        ),
+                        mode="markers",
+                        marker=dict(size=3, color='yellow'),
+                        name="Combo Signals",
+                    ),
+                ],
+            )
+            fig.update_xaxes(rangeslider_visible=False)
+            fig.update_layout(height=500, title="Last Column of the Results")
+            fig.show()
+   
     return pd.DataFrame(
         combine_array,
-        index=second_data.index,
+        index=second_eval_data.index,
         columns=pd.MultiIndex.from_tuples(
             tuples=list(pd_multind_tuples),
             names=pd_col_names,
