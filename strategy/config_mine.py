5fe791d020c588da94ccb3f5c1ccad056761dec0
quantfreedom <quantfreedom1022@gmail.com>
2023-04-19 09:01:31 -0700

    update git ignore

--- strategy/config_mine.py
+++
@@ -1,2 +0,0 @@
-api_key_test = '1gNbRkH99cvNihtgFI'
-api_secret_test = 'lKMdcsiuwv35baZFhSnQErneuEdK1tg6hHJ3'
--- strategy/pybit_test.py
+++
@@ -1,4 +0,0 @@
--(
-    (tpl * c * d + b * c * d - e * b * c + b * c * d * g + e * b * c * f)
-    / (d * (c - e + c * g + e * f))
-)
