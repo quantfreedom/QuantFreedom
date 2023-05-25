38fad6f6ff8f39bf4b14fdab3383b2d799de9f9f
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-05-25 11:59:34 -0700

    mine

--- quantfreedom/indicators/talib_ind.py
+++ quantfreedom/indicators/talib_ind.py
@@ -11,25 +11,24 @@ from collections.abc import Iterable
 
 from quantfreedom._typing import Array1d
 from quantfreedom.indicators.indicators_cls import Indicator
-from quantfreedom.plotting.simple_plots import (plot_on_candles_1_chart,
-                                                plot_results_candles_and_chart)
+from quantfreedom.plotting.simple_plots import (
+    plot_on_candles_1_chart,
+    plot_results_candles_and_chart,
+)
 
 # this is an update
+
 
 def validate(value, ref_name, ref_value):
     if not isinstance(value, list):
         raise ValueError(f"{ref_name} must be a list")
 
     if not all(isinstance(x, str) for x in value):
-        raise ValueError(
-            f"{ref_name} your list has to be made up of strings"
-        )
+        raise ValueError(f"{ref_name} your list has to be made up of strings")
 
     if len(value) != len(ref_value):
-        raise ValueError(
-            f"{ref_name} your list length must be {len(ref_value)}"
-        )
-    
+        raise ValueError(f"{ref_name} your list length must be {len(ref_value)}")
+
 
 def from_talib(
     func_name: str,
@@ -84,7 +83,11 @@ def from_talib(
     pd.DataFrame
         Pandas Dataframe of indicator values
     """
-    pd_index = indicator_data.index if indicator_data is not None and not indicator_data.empty else price_data.index
+    pd_index = (
+        indicator_data.index
+        if indicator_data is not None and not indicator_data.empty
+        else price_data.index
+    )
     indicator_info = Function(func_name).info
     output_names = indicator_info["output_names"]
     talib_func = getattr(talib, func_name.upper())
@@ -94,9 +97,11 @@ def from_talib(
 
     # Defaults to all possible combos, catesian product.
     if column_wise_combos:
-        params_len = [len(p) for p in indicator_info["parameters"].values() if isinstance(p, list)]
+        params_len = [
+            len(p) for p in indicator_info["parameters"].values() if isinstance(p, list)
+        ]
         lenghts = list(set(params_len))
-        if len(lenghts)>1:
+        if len(lenghts) > 1:
             raise ValueError("The length of the parameters needs to be the same.")
         base_lenght = lenghts[0]
         final_user_args = []
@@ -132,9 +137,11 @@ def from_talib(
         else:
             input_name_len = 1
         for input_name in input_names:
-            if input_name_len>1 and len(input_name) != input_name_len:
-                raise ValueError("The length of the input names must be the same as the indicator input names")
-            elif input_name_len==1:
+            if input_name_len > 1 and len(input_name) != input_name_len:
+                raise ValueError(
+                    "The length of the input names must be the same as the indicator input names"
+                )
+            elif input_name_len == 1:
                 input_name = [input_name]
             input_names_kwargs.append({input_name_key: input_name})
 
@@ -152,10 +159,12 @@ def from_talib(
         indexes = []
         for parameter in parameters_values:
             for combination in input_combinations:
-                indexes.append(combination+parameter)
+                indexes.append(combination + parameter)
 
         param_names = [f"{func_name}_{a}" for a in args_keys]
-        columns_index = pd.MultiIndex.from_tuples(indexes, names=("symbol", "output", "candle_body") + tuple(param_names))
+        columns_index = pd.MultiIndex.from_tuples(
+            indexes, names=("symbol", "output", "candle_body") + tuple(param_names)
+        )
         ta_lib_data = pd.DataFrame(columns=columns_index)
 
         for symbol in symbols:
@@ -169,33 +178,43 @@ def from_talib(
                     )
 
                     if output_names_len == 1:
-                        talib_output = (talib_output, )
+                        talib_output = (talib_output,)
 
                     for i, output_name in enumerate(output_names):
-                        ta_lib_data.loc[:, (symbol, output_name, json.dumps(input_name))+tuple(kwargs.values())] = talib_output[i]
+                        ta_lib_data.loc[
+                            :,
+                            (symbol, output_name, json.dumps(input_name))
+                            + tuple(kwargs.values()),
+                        ] = talib_output[i]
     elif indicator_data is not None:
         symbols = list(indicator_data.columns.levels[0])
         parameters_values = [tuple(d.values()) for d in user_kwargs]
         if isinstance(input_names[0], list):
-            raise("Indicator data does not support multiple inputs yet")
+            raise ("Indicator data does not support multiple inputs yet")
         else:
             input_names_json = [json.dumps(input_names)]
 
-
         talib_out = []
         for kwarg in user_kwargs:
-            indicator_output = indicator_data.apply(axis=0, raw=True, func=lambda x: talib_func(x.astype(np.float_), **kwarg))
-            index_values = [v + tuple(kwarg.values()) for v in indicator_output.columns.values]
+            indicator_output = indicator_data.apply(
+                axis=0,
+                raw=True,
+                func=lambda x: talib_func(x.astype(np.float_), **kwarg),
+            )
+            index_values = [
+                v + tuple(kwarg.values()) for v in indicator_output.columns.values
+            ]
             param_names = [f"{func_name}_{a}" for a in kwarg.keys()]
             index_names = indicator_output.columns.names + param_names
-            indicator_output.columns = pd.MultiIndex.from_tuples(index_values, names=index_names)
+            indicator_output.columns = pd.MultiIndex.from_tuples(
+                index_values, names=index_names
+            )
             talib_out.append(indicator_output)
         ta_lib_data = pd.concat(talib_out, axis=1)
 
     ta_lib_data.index = pd_index
     ta_lib_data.sort_index(axis=1, inplace=True)
     ta_lib_data.dropna(how="all", axis=0, inplace=True)
-
 
     if plot_on_data:
         if price_data is not None:
