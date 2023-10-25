46a0e3cd23bde48b51dee264e3fb5eaeec882ec5
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-10-24 20:23:49 -0700

    updates

--- nightvision/overlays/node_modules/fastify/test/types/import.ts
+++
@@ -1,1 +0,0 @@
-import { FastifyListenOptions, FastifyLogFn } from '../../fastify'
--- nightvision/overlays/node_modules/night-vision/node_modules/svelte/types/compiler/parse/read/css-tree-cq/node/lookahead_is_range.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Looks ahead to determine if query feature is a range query. This involves locating at least one delimiter and no
- * colon tokens.
- *
- * @returns {boolean} Is potential range query.
- */
-export declare function lookahead_is_range(): boolean;
