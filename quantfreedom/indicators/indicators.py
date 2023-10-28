70f84a1178c6b59da8d1e4afdaf2f93d76ad65a2
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-10-28 08:57:35 -0700

    got rsi working

--- quantfreedom/indicators/indicators.py
+++ quantfreedom/indicators/indicators.py
@@ -1,35 +1,26 @@
 import numpy as np
 
 
-def qf_calc_rsi(
-    prices: np.array,
-    period: int,
-):
-    prices_length = prices.size
-    up = np.zeros(prices_length)
-    down = np.zeros(prices_length)
-    mean_up = np.zeros(prices_length)
-    mean_down = np.zeros(prices_length)
-    rsi = np.full(prices_length, np.nan)
-    alpha = 1/period
-    for idx in range(1, period):
-        rma = alpha * prices[idx] + (1 - alpha) * prices[idx - 1]
-        pchg = (rma - prices[idx - 1]) / prices[idx - 1]
-        if pchg > 0:
-            up[idx] = pchg
-        else:
-            down[idx] = abs(pchg)
-    
-    
-    for idx in range(period, prices_length):
-        rma = (1 / period) * prices[idx] + (1 - (1 / period)) * prices[idx - 1]
-        pchg = (rma - prices[idx - 1]) / prices[idx - 1]
-        if pchg > 0:
-            up[idx] = pchg
-        else:
-            down[idx] = abs(pchg)
-        mean_up[idx] = np.mean(up[idx - period+1 : idx + 1])
-        mean_down[idx] = np.mean(down[idx - period+1 : idx + 1])
-        rsi[idx] = 100 - 100 / (1 + mean_up[idx] / mean_down[idx])
+def qf_calc_rsi(prices: np.array, period: int):
+    prices_shift = np.roll(prices, 1)
+    prices_shift[0] = np.nan
+    pchg = (prices - prices_shift) / prices_shift
 
+    alpha = 1 / period
+    gain = np.where(pchg > 0, pchg, 0)
+    avg_gain = np.full_like(gain, np.nan)
+
+    loss = np.where(pchg < 0, abs(pchg), 0)
+    avg_loss = np.full_like(loss, np.nan)
+
+    avg_gain[period] = gain[1 : period + 1].mean()
+    avg_loss[period] = loss[1 : period + 1].mean()
+
+    for i in range(period + 1, gain.size):
+        avg_gain[i] = alpha * gain[i] + (1 - alpha) * avg_gain[i - 1]
+        avg_loss[i] = alpha * loss[i] + (1 - alpha) * avg_loss[i - 1]
+
+    rs = avg_gain / avg_loss
+
+    rsi = 100 - (100 / (1 + rs))
     return rsi
