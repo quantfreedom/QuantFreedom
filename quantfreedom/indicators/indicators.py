7c66892f7de4292abc9425c8944bc7a72cf1d4f3
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-10-28 18:42:28 -0700

    updates

--- quantfreedom/indicators/indicators.py
+++ quantfreedom/indicators/indicators.py
@@ -7,21 +7,9 @@ def rsi_calc(source: np.array, length: int):
     pchgs = (source - prices_shift) / prices_shift
 
     gains = np.where(pchgs > 0, pchgs, 0)
-    rma_gains = np.full_like(gains, np.nan)
-
     losses = np.where(pchgs < 0, abs(pchgs), 0)
-    rma_losses = np.full_like(losses, np.nan)
 
-    rma_gains[length] = gains[1 : length + 1].mean()
-    rma_losses[length] = losses[1 : length + 1].mean()
-
-    rma_gains, rma_losses = rma_calc_2(
-        length=length,
-        s_1=gains,
-        rma_1=rma_gains,
-        s_2=losses,
-        rma_2=rma_losses,
-    )
+    rma_gains, rma_losses = rma_calc_2(length=length, source_1=gains, source_1=losses)
 
     rs = rma_gains / rma_losses
 
@@ -50,28 +38,29 @@ def sma_calc(source: np.array, length: int):
     return final
 
 
-def rma_calc_1(
-    length: int,
-    source: np.array,
-    rma: np.array,
-):
+def rma_calc_1(source: np.array, length: int):
     alpha = 1 / length
+
+    rma = np.full_like(source, np.nan)
+    rma[length] = source[1 : length + 1].mean()
+
     for i in range(length + 1, rma.size):
         rma[i] = alpha * source[i] + (1 - alpha) * rma[i - 1]
 
     return rma
 
 
-def rma_calc_2(
-    length: int,
-    s_1: np.array,
-    s_2: np.array,
-    rma_1: np.array,
-    rma_2: np.array,
-):
+def rma_calc_2(source_1: np.array, source_2: np.array, length: int):
     alpha = 1 / length
+
+    rma_1 = np.full_like(source_1, np.nan)
+    rma_2 = np.full_like(source_2, np.nan)
+
+    rma_1[length] = source_1[1 : length + 1].mean()
+    rma_2[length] = source_2[1 : length + 1].mean()
+
     for i in range(length + 1, rma_1.size):
-        rma_1[i] = alpha * s_1[i] + (1 - alpha) * rma_1[i - 1]
-        rma_2[i] = alpha * s_2[i] + (1 - alpha) * rma_2[i - 1]
+        rma_1[i] = alpha * source_1[i] + (1 - alpha) * rma_1[i - 1]
+        rma_2[i] = alpha * source_2[i] + (1 - alpha) * rma_2[i - 1]
 
     return rma_1, rma_2
