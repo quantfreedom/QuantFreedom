056e5dfa0cc6d0512710665641cc1c29c4a15a1f
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2023-11-06 18:32:12 -0800

    updates

--- quantfreedom/exchanges/apex_exchange/apex.py
+++ quantfreedom/exchanges/apex_exchange/apex.py
@@ -1,17 +1,4 @@
-import hashlib
-import hmac
-import base64
-import numpy as np
-from datetime import timedelta
-from requests import get, post
-from time import sleep, time
 from quantfreedom.exchanges.apex_exchange.apex_github.http_private_stark_key_sign import HttpPrivateStark
-from quantfreedom.exchanges.apex_exchange.apex_github.constants import NETWORKID_TEST, NETWORKID_MAIN
-from quantfreedom.enums import (
-    ExchangeSettings,
-    LeverageModeType,
-    PositionModeType,
-)
 from quantfreedom.exchanges.exchange import Exchange
 
 APEX_TIMEFRAMES = [1, 5, 15, 30, 60, 120, 240, 360, 720, "D", "W", "M"]
@@ -36,14 +23,11 @@ class Apex(Exchange):
 
         if use_test_net:
             url_start = "https://testnet.pro.apex.exchange"
-            # network_id = NETWORKID_TEST
         else:
             url_start = "https://pro.apex.exchange"
-            # network_id = NETWORKID_MAIN
 
         self.apex_stark = HttpPrivateStark(
             endpoint=url_start,
-            # network_id=network_id,
             stark_public_key=stark_key_public,
             stark_private_key=stark_key_private,
             stark_public_key_y_coordinate=stark_key_y,
