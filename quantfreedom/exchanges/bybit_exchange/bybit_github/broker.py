9e17f5b04dddb2b055733d9c81a3a3046715ae4d
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2024-03-02 11:56:31 -0800

    removed bybit github

--- quantfreedom/exchanges/bybit_exchange/bybit_github/broker.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Broker(str, Enum):
-    GET_BROKER_EARNINGS = "/v5/broker/earning-record"
-
-    def __str__(self) -> str:
-        return self.value
--- quantfreedom/exchanges/bybit_exchange/bybit_github/misc.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class Misc(str, Enum):
-    GET_ANNOUNCEMENT = "/v5/announcements/index"
-
-    def __str__(self) -> str:
-        return self.value
