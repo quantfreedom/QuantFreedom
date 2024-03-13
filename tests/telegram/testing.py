d441537c2fd3cc4b6e74b9c6874a6618ea949bba
Quant Freedom 1022 <quantfreedom1022@gmail.com>
2024-03-12 19:42:35 -0700

    updated testing

---
+++ tests/telegram/testing.py
@@ -0,0 +1,34 @@
+import asyncio
+
+from my_stuff import TELEGRAM_TOKEN
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.utils.markdown import hbold
+
+
+dp = Dispatcher()
+
+"https://t.me/quantfreedom_bot"
+
+
+@dp.message(CommandStart())
+async def cmd_start(msg: types.Message) -> None:
+    reply_text = f"Hello, {hbold(msg.from_user.username)}"
+
+    await msg.answer(
+        text=reply_text,
+    )
+
+
+async def main() -> None:
+    bot = Bot(
+        token=TELEGRAM_TOKEN,
+        parse_mode="HTML",
+    )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
